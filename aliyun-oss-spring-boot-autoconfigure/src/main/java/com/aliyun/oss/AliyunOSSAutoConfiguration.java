6b43617486eb057fcbf440d417815a6faf329eec
Hjh <2256718717@qq.com>
2026-07-31 07:41:38 +0800

    bean

--- aliyun-oss-spring-boot-autoconfigure/src/main/java/com/aliyun/oss/AliyunOSSAutoConfiguration.java
+++ aliyun-oss-spring-boot-autoconfigure/src/main/java/com/aliyun/oss/AliyunOSSAutoConfiguration.java
@@ -5,6 +5,18 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+//自定义starter
+//●需求：自定义aliyun-oss-spring-boot-starter，完成阿里云oss操作工具类AliyunOSSOperator的自动配置。
+//●目标：引入起步依赖引入之后，要想使用阿里云oss，注入Aliyunossoperator直接使用即可。
+//● 步骤:
+//1.创建aliyun-oss-spring-boot-starter模块(只需配置autoconfigure的依赖)
+//2.创建aliyun-oss-spring-boot-autoconfigure模块，在starter中引入该模块
+//3.在aliyun-oss-spring-boot-autoconfigure模块中的定义自动配置功能(引入oss依赖，并编写上传功能
+// 编写AutoConfiguration引入bean用于注入，如果bean还存在依赖关系，通过EnableConfigurationProperties
+// 注解import注入)，
+// 并定义自动配置文件META-INF/spring/xxxx.imports
+
+
 
 /**
  * AliyunOSS的自动配置类
--- springboot-autoconfiguration-test/src/main/java/com/itheima/controller/UploadController.java
+++ springboot-autoconfiguration-test/src/main/java/com/itheima/controller/UploadController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+/**
+ * 用于测试自定义starter 文件上传*/
 
 @RestController
 public class UploadController {
@@ -16,7 +18,7 @@ public class UploadController {
     public String upload(MultipartFile image) throws Exception {
         //上传文件到阿里云 OSS
 
-        //引入依赖
+        //引入自定义的依赖，并配置配置文件
 
         String url = aliyunOSSOperator.upload(image.getBytes(), image.getOriginalFilename());
         System.out.println( url);
